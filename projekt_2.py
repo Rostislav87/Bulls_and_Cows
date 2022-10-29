4d5844d9898e9595ac4fd6218039ec60bdfb1c48
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-10-29 10:44:13 +0200

    Ctvrty commit

--- projekt_2.py
+++ projekt_2.py
@@ -20,21 +20,21 @@ def uvitej_uzivatele():
     """)
 
 
+def main():
+    game = True
+    hra(game)
+
 # Počítačem náhodně vygenerovaná 4 unikátní čísla 
 
 
-def vytvor_nahodne_cislo():
+def hra(game):
     number = random.sample(range(10), 4)
     if number[0] != 0:
         random_number = str(number[0]) + str(number[1]) + str(number[2]) + str(number[3])
-        return random_number
+        print(random_number)
 
 
 # Uživatel hádá číslo. Program jej upozorní, pokud zadá číslo kratší nebo delší než 4 čísla. Pokud bude obsahovat duplicity, začínat nulou, příp. obsahovat nečíselné znaky 
-
-
-def vstup_uzivatele():
-    game = True
     while game:
         player_number = input("Enter a number: ")
         print("-----------------------------------------------")
@@ -44,16 +44,25 @@ def vstup_uzivatele():
             print("Your first number can't be 0.")
         elif len(player_number) != len(set(player_number)):
             print("You have to input 4 different digits.") 
+        
+        # Program dále vypíše počet bull/ bulls (pokud uživatel uhodne jak číslo, tak jeho umístění), příp. cows/ cows (pokud uživatel uhodne pouze číslo, ale ne jeho umístění). Vrácené ohodnocení musí brát ohled na jednotné a množné číslo ve výstupu. Tedy 1 bull a 2 bulls (stejně pro cow/cows)
+        bull = 0
+        cow = 0
+        for num, num2 in zip(random_number, player_number):
+            if num2 in random_number:
+                if num2 == num:
+                    bull += 1
+                else:
+                    cow += 1            
+        print(f"Bulls: {bull} Cows: {cow}")   
+        if bull == 4:
+            game = False
+            print("Congratulations. That's a right number. You won!")
 
 
-    # Program vyhodnotí tip uživatele.
-    
-    
-    
-    
-    # Program dále vypíše počet bull/ bulls (pokud uživatel uhodne jak číslo, tak jeho umístění), příp. cows/ cows (pokud uživatel uhodne pouze číslo, ale ne jeho umístění). Vrácené ohodnocení musí brát ohled na jednotné a množné číslo ve výstupu. Tedy 1 bull a 2 bulls (stejně pro cow/cows)
 
-
+uvitej_uzivatele()
+main()
 
 
     
