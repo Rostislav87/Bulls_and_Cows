913ec13e3a2644315357c675f37c9d1f06d83b1d
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-11-08 13:18:23 +0100

    Přidání funkce os.system("cls"), pokud uživatel při zadávání čísla zadá klávesu "q" pro ukončení hry.

--- projekt_2.py
+++ projekt_2.py
@@ -31,6 +31,7 @@ def generate_number():
     if number[0] != 0:
         for num in number:
             random_number += str(num)
+
     return random_number    
 
 
@@ -43,6 +44,7 @@ def play_game():
         player_number = input("Enter a number or 'q' for exit the game: ")
         print(separator)
         if player_number == "q":
+            os.system("cls")
             break
         elif len(player_number) != 4 or not player_number.isdigit():
             print("Your input is not 4 digit number. Write only number. Enter again.")
